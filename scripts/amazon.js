6b07219211adf394420c0016603426ed8885537f
FernandaMello <fernandam04@outlook.com>
2023-11-03 13:17:43 -0300

    initial creation of cart data

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -48,7 +48,7 @@ products.forEach((product)=>{
         Added
         </div>
 
-        <button class="add-to-cart-button button-primary js-add-to-cart">
+        <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">
         Add to Cart
         </button>
     </div>
@@ -62,6 +62,11 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) =>{
     button.addEventListener('click', ()=>{
-        console.log('Add product')
-    })
-})
+        const productName = button.dataset.productName;
+        cart.push({
+            productName: productName,
+            quantity: 1
+        });
+        console.log(cart);
+    });
+});
